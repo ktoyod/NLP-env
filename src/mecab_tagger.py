01b15f143726e74ca2c6b97f139e7a2f265ca41f
ktoyod <ytdh7834@gmail.com>
2019-08-22 14:33:04 +0900

    add some files

---
+++ src/mecab_tagger.py
@@ -0,0 +1,15 @@
+import MeCab
+
+
+def mecab_tagger(sys_dic_path=None, user_dic_path=None):
+    if sys_dic_path and user_dic_path:
+        mecab = MeCab.Tagger(f'-d {sys_dic_path} -u {user_dic_path}')
+    elif sys_dic_path:
+        mecab = MeCab.Tagger(f'-d {sys_dic_path}')
+    elif user_dic_path:
+        mecab = MeCab.Tagger(f'-u {user_dic_path}')
+    else:
+        mecab = MeCab.Tagger
+    mecab.parse('')
+
+    return mecab
---
+++ src/path.py
@@ -0,0 +1,5 @@
+import os
+
+
+class Path(object):
+    NEOLOGD_PATH = '/usr/local/lib/mecab/dic/mecab-ipadic-neologd'
